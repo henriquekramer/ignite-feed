ef3575375fb5b7f4c40ea54f96cf77a96cdde0db
Henrique Kramer <henriquemkramer@gmail.com>
2022-07-05 19:58:20 -0300

    Criar comentários

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,42 @@ import { Post } from './components/Post'
 import { Header } from './components/Header' 
 import { Sidebar } from './components/Sidebar' 
 
-
 import './global.css'
 import styles from './App.module.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/henriquekramer.png',
+      name: 'Henrique Kramer',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+      { type: 'link', content: '#novoprojeto #nlw #rocketseat'},
+    ],
+    publishedAt: new Date('2022-07-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+      { type: 'link', content: '#novoprojeto #nlw #rocketseat'},
+
+    ],
+    publishedAt: new Date('2022-07-04 20:00:00'),
+  }
+]
 
 
 export function App() {
@@ -14,18 +47,19 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        
         <main>
-          <Post 
-            author="Henrique Kramer"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, quisquam."
-          />
-          <Post 
-            author="Henrique Kramer"
-            content="Um novo texto.."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id} 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}              
+              />
+            )
+          })}
+          
         </main>
-
       </div>
 
     </div>
